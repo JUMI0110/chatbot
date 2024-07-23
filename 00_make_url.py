c5c5b9d9ab2b41724aff14246a1962a0258a6f71
jumi <ajsel422@gmail.com>
2024-07-23 11:29:38 +0900

    make url

---
+++ 00_make_url.py
@@ -0,0 +1,25 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+URL = f'https://api.telegram.org/bot{TOKEN}'
+
+method = 'getUpdates'
+print(f'{URL}/{method}')
+
+res = requests.get(f'{URL}/{method}')
+
+res_dict = res.json()
+
+user_input = res_dict['result'][-1]['message']['text']
+user_id = res_dict['result'][-1]['message']['from']['id']
+
+print(user_id, user_input)
+
+SEND_MSG_URL = f'{URL}/sendMessage?chat_id={user_id}&text={user_input}'
+
+requests.get(SEND_MSG_URL)
